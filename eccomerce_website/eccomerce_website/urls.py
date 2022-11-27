9de0c87c51641d1e41cbc1f4d48965009487dc03
julialaska <julkalas101@gmail.com>
2022-11-27 22:30:22 +0100

    view change

--- eccomerce_website/eccomerce_website/urls.py
+++ eccomerce_website/eccomerce_website/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from websiteapp.views import frontpage
+from websiteapp.views import frontpage, shop
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('shop/', shop, name='shop'),
     path('admin/', admin.site.urls),
 ]
--- eccomerce_website/websiteapp/views.py
+++ eccomerce_website/websiteapp/views.py
@@ -5,5 +5,12 @@ from product.models import Product
 
 
 def frontpage(request):
-    products = Product.objects.all()[0:8]
+    products = Product.objects.all()[0:6]
+
     return render(request, 'website_app/frontpage.html', {'products': products})
+
+
+def shop(request):
+    products = Product.objects.all()
+
+    return render(request, 'website_app/shop.html', {'products': products})
