b6b9f8cb667f1ddc8781ea9023238a125fc437b1
julialaska <julkalas101@gmail.com>
2022-12-03 17:26:13 +0100

    improve product get object or 404

--- eccomerce_website/product/views.py
+++ eccomerce_website/product/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from product.models import Product
 
 
 def product(request, slug):
-    product = Product.objects.get(slug=slug)
+    product = get_object_or_404(Product, slug=slug)
 
     return render(request, 'product/product.html', {'product': product})
