b42dbcf54e5ba8f247767b9b206437ecd84ef450
julialaska <julkalas101@gmail.com>
2022-12-03 17:23:47 +0100

    product detail sites

--- eccomerce_website/product/views.py
+++ eccomerce_website/product/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from product.models import Product
 
-def product(request):
-    return render(request, 'product/product.html')
+
+def product(request, slug):
+    product = Product.objects.get(slug=slug)
+
+    return render(request, 'product/product.html', {'product': product})
