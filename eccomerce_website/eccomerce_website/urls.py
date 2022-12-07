da9026f80f065c406dc17cfeb7a0fbaf222376cc
julialaska <julkalas101@gmail.com>
2022-12-07 15:56:23 +0100

    create view for cart

--- eccomerce_website/eccomerce_website/urls.py
+++ eccomerce_website/eccomerce_website/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 
 from websiteapp.views import frontpage, shop
 from product.views import product
+from cart.views import add_to_cart
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
+    path('add_to_cart/<int:product_id>', add_to_cart, name="add_to_cart"),
     path('admin/', admin.site.urls),
 ]
